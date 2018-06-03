c67cfbb529e76bc8423a9ac0e289ffdcd04a743e
ymmhuasuo <308458093@qq.com>
2018-06-03 21:04:48 +0800

    test

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'antd-mobile';
+import { Button, Icon, Radio } from 'antd-mobile';
+
+const RadioItem = Radio.RadioItem;
 
 export default class App extends React.Component {
   render() {
@@ -8,6 +10,9 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Changes you m automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <RadioItem key={1} checked={true} >
+          "i.label"
+        </RadioItem>
         <Button>antd-mobile button</Button>
       </View>
     );
